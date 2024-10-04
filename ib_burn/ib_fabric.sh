0bc66043666ef0b19dba8597bbb35d72903a05eb
Jakub Piotr Cłapa <jakub@hume.ai>
2024-10-04 18:04:57 +0000

    ib_fabric: add some debug output and use sudo on ib commands

--- ib_burn/ib_fabric.sh
+++ ib_burn/ib_fabric.sh
@@ -25,11 +25,13 @@ do
     SSH_ADDR=$(read_config '.node_info.nodes[$ARG]' "$SSH_HOST")
     for IB_HCA in $(read_config '.ib_hcas | keys[]')
     do
+	echo $SSH_ADDR
         IB_GUID=$(ssh "$SSH_USER@$SSH_ADDR" "ibstat --short $IB_HCA | grep Node | tr ' ' '\t' | cut -f 4")
         echo "$IB_GUID $SSH_HOST $IB_HCA" >> "$IB_SERVER_LIST"
     done
 done
 
-ssh "$SSH_USER@$SSH_ADDR" 'ibswitches' | tr -d '"' | awk '{ print($3, $6) }' > "$IB_SWITCH_LIST"
-ssh "$SSH_USER@$SSH_ADDR" 'iblinkinfo --line --switches-only' | grep 'Active/' \
+echo Switches: "$SSH_ADDR"
+ssh "$SSH_USER@$SSH_ADDR" 'sudo ibswitches' | tr -d '"' | awk '{ print($3, $6) }' > "$IB_SWITCH_LIST"
+ssh "$SSH_USER@$SSH_ADDR" 'sudo iblinkinfo --line --switches-only' | grep 'Active/' \
     | sed -E 's/"[^"]*"//g;s/\([^)]+\)//g;s/\[[^]]*\]//g' | awk '{ print($1, $5, $3, $7) }' > "$IB_SWITCH_LINK"
